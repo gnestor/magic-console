88a99c328983f67995944dfa261da6aeac30b984
Grant Nestor <grantnestor@gmail.com>
2016-04-20 18:41:06 -0700

    Update ReactComponent plugin
    * Pass the path to any React component and props to ReactComponent to
    render any React component
    * Pass callback functions as strings via props to define component
    behavior from source file (callbacks have access to the `global` object)

--- plugins/ReactComponent.js
+++ plugins/ReactComponent.js
@@ -2,28 +2,77 @@
 
 import React, {Component, PropTypes} from 'react'
 import vm from 'vm'
-import path from 'path'
+import fs from 'fs'
 const babel = require('babel-core')
 
 class ReactComponent extends Component {
 
-  render() {
-    let {type, props = {}} = this.props.data
-    if (type) {
-      if (React.DOM[type]) return React.createElement(type, props)
-      let transpiled = babel.transform(type).code
-      let evaluated = vm.runInNewContext(transpiled, {React})
-      // return React.createElement(vm.runInNewContext(type, {React}), props)
-      return React.createElement(evaluated, props)
+  constructor(props) {
+    super(props)
+    this.component = null
+    this.watch = null
+    this.requireComponent = this.requireComponent.bind(this)
+    this.watchComponent = this.watchComponent.bind(this)
+  }
+
+  componentWillMount() {
+    let {path} = this.props.data
+    this.requireComponent(path)
+    this.watch = this.watchComponent(path)
+  }
+
+  componentWillUnmount() {
+    this.watch.close()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let {path} = nextProps.data
+    if (path !== this.props.data.path) {
+      this.watch.close()
+      this.requireComponent(path)
+      this.watch = this.watchComponent(path)
     }
+  }
+
+  render() {
+    let {props} = this.props.data
+    if (props) {
+      props = Object.keys(props).reduce((props, key) => {
+        let prop = props[key]
+        try {
+          let {code} = babel.transform(prop)
+          let callback = vm.runInThisContext(code)
+          return {...props, [key]: callback}
+        } catch (error) {
+          return props
+        }
+      }, props)
+    }
+    return React.createElement(this.component, props)
+  }
+
+  requireComponent(path) {
+    try {
+      this.component = require(path)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  watchComponent(path) {
+    return fs.watch(path, (event, file) => {
+      console.log('%cwatchComponents', 'font-weight: bold', file)
+      delete require.cache[require.resolve(path)]
+      this.requireComponent(path)
+    })
   }
 
 }
 
 ReactComponent.propTypes = {
   data: PropTypes.shape({
-    type: PropTypes.string.isRequired,
-    props: PropTypes.object.isRequired
+    path: PropTypes.string.isRequired,
+    props: PropTypes.object
   }).isRequired
 }
 
---
+++ tests/ReactComponent/EditableTable.js
@@ -0,0 +1,90 @@
+'use babel'
+
+import React, {Component, PropTypes} from 'react'
+import ReactDataGrid from 'react-data-grid/addons'
+
+class EditableTable extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      rows: [],
+      selected: []
+    }
+    this.handleSort = this.handleSort.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({rows: this.props.data})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({rows: nextProps.data})
+  }
+
+  render() {
+    let columns = this.state.rows.reduce((columns, row) => {
+      if (typeof row === 'object') {
+        return Object.keys(row).reduce((columns, key) => ([...columns.filter(column => column.key !== key), {
+          key,
+          name: key,
+          resizable: true,
+          sortable: true,
+          editable: true
+        }]), columns)
+      } else {
+        return {
+          key: 'default',
+          name: ''
+        }
+      }
+    }, [])
+    let rows = this.state.rows.map(row => (
+      columns.reduce((row, column) => {
+        let value = row[column.key]
+        if (typeof row[column.key] === 'object') value = `${row[column.key]}`
+        if (!row[column.key]) value = ''
+        return {
+          ...row,
+          [column.key]: value
+        }
+      }, row)
+    ))
+    return <ReactDataGrid
+      columns={columns}
+      rowGetter={index => rows[index]}
+      rowKey="key"
+      rowsCount={this.state.rows.length}
+      enableRowSelect="multi"
+      minHeight={(this.state.rows.length + 1) * 35}
+      onGridSort={this.props.handleSort || this.handleSort}
+      onRowSelect={this.props.handleSelect || this.handleSelect}
+    />
+  }
+
+  handleSort(column, direction) {
+    if (direction !== 'NONE') {
+      let rows = this.state.rows.sort((a, b) => {
+        if (direction === 'ASC') {
+          return (a[column] > b[column]) ? 1 : -1
+        } else if (direction === 'DESC') {
+          return (a[column] < b[column]) ? 1 : -1
+        }
+      })
+      this.setState({rows})
+    }
+  }
+
+  handleSelect(rows) {
+    // console.log(rows)
+    this.setState({selected: rows})
+  }
+
+}
+
+EditableTable.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default EditableTable
